e12bfac0d413d7dc7b604c851c761e6c840c0495
Sand Stone <sand.m.stone@gmail.com>
2016-08-20 11:42:35 -0700

    add mysql table meta-data read util

---
+++ slipstream/src/main/java/slipstream/MySQLMetaData.java
@@ -0,0 +1,50 @@
+package slipstream;
+
+import java.sql.*;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+public class MySQLMetaData {
+  private static Logger log = LogManager.getLogger(MySQLApplier.class);
+  private final static String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+
+  String url;
+  String user;
+  String password;
+
+  public MySQLMetaData(String url, String user, String password) {
+    this.url = url;
+    this.user = user;
+    this.password = password;
+  }
+
+  public void get() throws SQLException, ClassNotFoundException {
+    Class.forName("com.mysql.jdbc.Driver");
+    Connection connection = DriverManager.getConnection(url, user, password);
+    DatabaseMetaData metaData = connection.getMetaData();
+    ResultSet tables = metaData.getTables(null, "public", null, new String[]{"TABLE"});
+    try {
+      while (tables.next()) {
+        String tableName = tables.getString("TABLE_NAME");
+        log.info("table = {}", tableName);
+        ResultSet cols = metaData.getColumns(null, "public", tableName, null);
+        try {
+          while (cols.next()) {
+            String columnName = cols.getString("COLUMN_NAME");
+            String columnType = cols.getString("TYPE_NAME").toLowerCase();
+            log.info("col {}={}", columnName, columnType);
+          }
+        } finally {
+          cols.close();
+        }
+      }
+    } finally {
+      tables.close();
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    new MySQLMetaData("jdbc:mysql://localhost/acme?autoReconnect=true&useSSL=false", "root", "mysql").get();
+
+  }
+}
