b57990904bcaa6e8c0a1907f2d69d4d7d568f842
Sand Stone <sand.m.stone@gmail.com>
2016-09-06 22:01:21 -0700

    rename DataTable

--- slipstream/src/main/java/slipstream/MetaDataTable.java
+++ slipstream/src/main/java/slipstream/MetaDataTable.java
@@ -10,16 +10,17 @@ import java.util.concurrent.*;
 import java.time.*;
 import com.wiredtiger.db.*;
 
-class DataTable {
+class MetaDataTable {
   final static String datasource = "table:datasource";
   final static String ensemble = "table:ensemble";
   final static String shard = "table:shard";
+  final static String schema = "table:schema";
   private static final String storage_format = "key_format=S,value_format=u";
 
-  private static Logger log = LogManager.getLogger(DataTable.class);  
+  private static Logger log = LogManager.getLogger(MetaDataTable.class);
   Connection conn;
 
-  public DataTable(String db) {
+  public MetaDataTable(String db) {
     Util.checkDir(db);
     conn = wiredtiger.open(db, "create");
     Session session = conn.open_session(null);
@@ -28,6 +29,8 @@ class DataTable {
     ret = session.create(ensemble, storage_format);
     check(ret);
     ret = session.create(shard, storage_format);
+    check(ret);
+    ret = session.create(schema, "key_format=SS,value_format=S");
     check(ret);
     session.close(null);
   }
@@ -40,5 +43,5 @@ class DataTable {
   public void close() {
     conn.close(null);
   }
-  
+
 }
