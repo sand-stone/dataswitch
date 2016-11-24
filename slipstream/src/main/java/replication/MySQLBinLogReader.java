3aad2489eb03cef449d9f4b1b6ec691042142332
Sand Stone <sand.m.stone@gmail.com>
2016-11-24 08:18:05 -0800

    code refactor starts

---
+++ slipstream/src/main/java/replication/MySQLBinLogReader.java
@@ -0,0 +1,104 @@
+package slipstream;
+
+import java.nio.ByteBuffer;
+import java.nio.file.*;
+import java.nio.file.StandardWatchEventKinds;
+import java.io.*;
+import java.util.*;
+import java.net.*;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import com.github.shyiko.mysql.binlog.BinaryLogFileReader;
+import com.github.shyiko.mysql.binlog.event.ByteArrayEventData;
+import com.github.shyiko.mysql.binlog.event.*;
+import com.github.shyiko.mysql.binlog.event.deserialization.ByteArrayEventDataDeserializer;
+import com.github.shyiko.mysql.binlog.event.deserialization.EventDeserializer;
+import com.github.shyiko.mysql.binlog.event.deserialization.NullEventDataDeserializer;
+import org.apache.commons.configuration2.*;
+import org.apache.commons.configuration2.builder.fluent.Configurations;
+import java.util.concurrent.Future;
+import slipstream.DBTransactionEvent.FieldType;
+import slipstream.DBTransactionEvent.OperationType;
+
+class MySQLBinLogReader {
+  private static Logger log = LogManager.getLogger(MySQLBinLogReader.class);
+
+  public MySQLBinLogReader() {
+  }
+
+  private FieldType[] getFieldTypes(TableMapEventData mapEvent) {
+    byte[] types = mapEvent.getColumnTypes();
+    FieldType[] ftypes = new FieldType[types.length];
+    for(int i = 0; i < types.length; i++) {
+      ftypes[i] = FieldType.map(types[i]);
+    }
+    return ftypes;
+  }
+
+  private OperationType getOpType(EventData evt) {
+    OperationType ret = OperationType.None;
+    if (evt instanceof WriteRowsEventData) {
+      ret = OperationType.Insert;
+    } else if (evt instanceof UpdateRowsEventData) {
+      ret = OperationType.Update;
+    }
+    return ret;
+  }
+
+  private Object[] getRows(EventData evt) {
+    Object[] rows = null;
+    if(evt instanceof UpdateRowsEventData)
+      rows = ((UpdateRowsEventData)evt).getRows().toArray();
+    else if(evt instanceof WriteRowsEventData)
+      rows = ((WriteRowsEventData)evt).getRows().toArray();
+    return rows;
+  }
+
+  private BitSet getIncludedCols(EventData evt) {
+    BitSet ret = null;
+    if(evt instanceof UpdateRowsEventData)
+      ret = ((UpdateRowsEventData)evt).getIncludedColumns();
+    else if(evt instanceof WriteRowsEventData)
+      ret = ((WriteRowsEventData)evt).getIncludedColumns();
+    return ret;
+  }
+
+  public void process(InputStream input) throws IOException {
+    EventDeserializer eventDeserializer = new EventDeserializer();
+    eventDeserializer.setEventDataDeserializer(EventType.XID, new ByteArrayEventDataDeserializer());
+    eventDeserializer.setEventDataDeserializer(EventType.QUERY, new ByteArrayEventDataDeserializer());
+    BinaryLogFileReader reader = new BinaryLogFileReader(input, eventDeserializer);
+    try {
+      TableMapEventData mapEvent = null; EventData crudEvent = null;
+      for (Event dbevent; (dbevent = reader.readEvent()) != null; ) {
+        EventType eventType = dbevent.getHeader().getEventType();
+        switch(eventType) {
+        case TABLE_MAP:
+          mapEvent = (TableMapEventData)dbevent.getData();
+          break;
+        case EXT_WRITE_ROWS:
+        case EXT_UPDATE_ROWS:
+        case EXT_DELETE_ROWS:
+          crudEvent = dbevent.getData();
+          break;
+        case XID:
+          EventHeaderV4 header = dbevent.getHeader();
+          DBTransactionEvent evt = new DBTransactionEvent(header.getServerId(),
+                                                          mapEvent.getDatabase(),
+                                                          mapEvent.getTable(),
+                                                          getOpType(crudEvent),
+                                                          header.getTimestamp(),
+                                                          header.getNextPosition(),
+                                                          getIncludedCols(crudEvent),
+                                                          getFieldTypes(mapEvent),
+                                                          getRows(crudEvent));
+          crudEvent = null;
+          mapEvent = null;
+        }
+      }
+    } finally {
+      reader.close();
+    }
+  }
+
+}
---
+++ slipstream/src/main/java/replication/MySQLWriter.java
@@ -0,0 +1,48 @@
+package replication;
+
+import java.sql.*;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+public class MySQLWriter implements Runnable {
+  private static Logger log = LogManager.getLogger(MySQLWriter.class);
+  private final static String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+
+  String url;
+  String user;
+  String password;
+
+  public MySQLWriter(String url, String user, String password) {
+    this.url = url;
+    this.user = user;
+    this.password = password;
+  }
+
+  private Connection getSQLConn() throws Exception {
+    Class.forName("com.mysql.jdbc.Driver");
+    return DriverManager.getConnection(url, user, password);
+  }
+
+  private void insert(Object evt) throws SQLException {
+    //log.info("sql string:"+ evt.getSQL(getSchema(evt.database, evt.table)));
+  }
+
+  public void run() {
+    try {
+      log.info("mysql writer started");
+      Connection mysqlconn = getSQLConn();
+      while(true) {
+        insert(null);
+      }
+    } catch(Exception e) {
+      log.error(e);
+      e.printStackTrace();
+    } finally {
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    new MySQLWriter("jdbc:mysql://localhost/acme?autoReconnect=true&useSSL=false", "root", "mysql").run();
+  }
+
+}
