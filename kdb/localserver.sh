0ede6b28eb7aa207c55a03ba16df798ab5849e73
Sand Stone <sand.m.stone@gmail.com>
2017-01-02 18:21:39 -0800

    localserver

--- kdb/localserver.sh
+++ kdb/localserver.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-java -server -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -cp target/kdb-1.0-SNAPSHOT.jar kdb.Transport conf/datanode1.properties &
-java -server -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -cp target/kdb-1.0-SNAPSHOT.jar kdb.Transport conf/datanode2.properties &
-java -server -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -cp target/kdb-1.0-SNAPSHOT.jar kdb.Transport conf/datanode3.properties &
+nohup java -server -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -cp target/kdb-1.0-SNAPSHOT.jar kdb.Transport conf/datanode1.properties &
+nohup java -server -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -cp target/kdb-1.0-SNAPSHOT.jar kdb.Transport conf/datanode2.properties &
+nohup java -server -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -cp target/kdb-1.0-SNAPSHOT.jar kdb.Transport conf/datanode3.properties &
