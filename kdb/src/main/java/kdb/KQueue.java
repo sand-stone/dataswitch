4d27a407ec85205f1192aecc1d1c18b4865b00ba
Sand Stone <sand.m.stone@gmail.com>
2017-01-05 02:07:37 -0800

    ensure correctness

--- kdb/src/main/java/kdb/KQueue.java
+++ kdb/src/main/java/kdb/KQueue.java
@@ -29,7 +29,7 @@ class KQueue implements Closeable {
   Thread[] workers;
 
   private KQueue() {
-    workers = new Thread[17];
+    workers = new Thread[23];
     queues = new Object[workers.length];
     for(int i = 0; i < workers.length; i++) {
       queues[i] = new LinkedBlockingQueue<>(MAX_PENDING_REQS);
@@ -57,44 +57,25 @@ class KQueue implements Closeable {
     }
   }
 
-  private static class WorkItem {
-    String table;
-    Client.Result rsp;
-
-    public WorkItem(String table, Client.Result rsp) {
-      this.table = table;
-      this.rsp = rsp;
-    }
-  }
-
   private class Updater implements Runnable {
     Client client;
     LinkedBlockingQueue<SequenceOperation> queue;
-    LinkedBlockingQueue<WorkItem> workq;
-
-    private class Worker implements Runnable {
-      public void run() {
-        while(true) {
-          try {
-            WorkItem item = workq.take();
-            Store.get().update(item.table, item.rsp);
-          } catch(InterruptedException e) {
-          }
-        }
-      }
-    }
 
     public Updater(int bucket) {
       client = new Client();
       this.queue = (LinkedBlockingQueue<SequenceOperation>)queues[bucket];
-      this.workq = new LinkedBlockingQueue<>(1);
-      new Thread(new Worker()).start();
     }
 
     public void run() {
       while(true) {
         try {
           SequenceOperation op = queue.take();
+          if(queue.size() > 0) {
+            int count = queue.size();
+            while(count--> 0) {
+              op = queue.take();
+            }
+          }
           long seqno = op.getSeqno();
           Store.DataTable dt = Store.get().tables.get(op.getTable());
           long lsn = dt.db.getLatestSequenceNumber();
@@ -104,9 +85,7 @@ class KQueue implements Closeable {
             int limit = delta < 1000? delta : 1000;
             Client.Result rsp = client.scanlog("http://"+op.getEndpoint(), op.getTable(), lsn, limit);
             //log.info("target {} fetch wal table {} rsp count {} seqno {}", seqno, op.getTable(), rsp.count(), rsp.seqno());
-            //lsn = Math.max(Store.get().update(op.getTable(), rsp), rsp.seqno() + 1);
-            workq.put(new WorkItem(op.getTable(), rsp));
-            lsn = rsp.seqno() + 1;
+            lsn = Math.max(Store.get().update(op.getTable(), rsp), rsp.seqno() + 1);
             //log.info("table {} seqno {} lsn {}", op.getTable(), seqno, lsn);
           }
         } catch(Exception e) {
