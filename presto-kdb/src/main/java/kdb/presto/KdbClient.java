eb06d91cfd269c7cea4c491ed06c8f7834a460bd
Sand Stone <sand.m.stone@gmail.com>
2017-01-17 15:39:36 -0800

    init presto connector

---
+++ presto-kdb/src/main/java/kdb/presto/KdbClient.java
@@ -0,0 +1,34 @@
+package kdb.presto;
+
+import com.google.common.base.Function;
+import com.google.common.base.Supplier;
+import com.google.common.base.Suppliers;
+import com.google.common.base.Throwables;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.io.Resources;
+import io.airlift.json.JsonCodec;
+import io.airlift.log.Logger;
+
+import javax.inject.Inject;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static com.google.common.collect.Iterables.transform;
+import static com.google.common.collect.Maps.transformValues;
+import static com.google.common.collect.Maps.uniqueIndex;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.util.Objects.requireNonNull;
+
+import kdb.Client;
+
+public class KdbClient {
+  private static final Logger log = Logger.get(KdbClient.class);
+
+}
