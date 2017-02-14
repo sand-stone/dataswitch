bda13ab8750681b105d85df943ea043c444cbed9
Sand Stone <sand.m.stone@gmail.com>
2017-02-14 09:12:32 -0800

    set memtable size

--- kdb/src/main/java/kdb/Store.java
+++ kdb/src/main/java/kdb/Store.java
@@ -232,6 +232,9 @@ class Store implements Closeable {
           case "MaxBackgroundCompactions":
             options.setMaxBackgroundCompactions(toInt(v));
             break;
+          case "WriteBufferSize":
+            options.setWriteBufferSize(toLong(v));
+            break;
           case "MaxWriteBufferNumber":
             options.setMaxWriteBufferNumber(toInt(v));
             break;
